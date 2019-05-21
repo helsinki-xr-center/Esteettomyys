4bf9cae1d670778d21cb3f92f14bce8aff419fb9
Nominom <nomi.lakkala@gmail.com>
2019-05-21 14:10:11 +0300

    Added VRModeEnabler script for testing

---
+++ Assets/Scripts/VRModeEnabler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Author: Nomi Lakkala
+ * 
+ * Enables or disables the VR mode on Start.
+ */
+public class VRModeEnabler : MonoBehaviour
+{
+
+	public bool vrEnabled = true;
+
+	void Start()
+	{
+		UnityEngine.XR.XRSettings.enabled = vrEnabled;
+	}
+
+}
