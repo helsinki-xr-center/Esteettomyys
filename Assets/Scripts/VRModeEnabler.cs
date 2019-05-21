a3f857c6e0a5aa29657511c21b6cbfb1f12bb34f
Nominom <nomi.lakkala@gmail.com>
2019-05-21 14:30:27 +0300

    Added VRModeEnabler to MainMenu. Added check to VRModeEnabler for if VRDevice is not present

--- Assets/Scripts/VRModeEnabler.cs
+++ Assets/Scripts/VRModeEnabler.cs
@@ -14,7 +14,12 @@ public class VRModeEnabler : MonoBehaviour
 
 	void Start()
 	{
-		UnityEngine.XR.XRSettings.enabled = vrEnabled;
+		if(!UnityEngine.XR.XRDevice.isPresent && vrEnabled){
+			Debug.LogError("No VR device present while trying to enable VR.", this);
+		}else{
+			UnityEngine.XR.XRSettings.enabled = vrEnabled;
+
+		}
 	}
 
 }
