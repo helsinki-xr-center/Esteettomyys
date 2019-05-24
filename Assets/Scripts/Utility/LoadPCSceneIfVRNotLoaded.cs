05ba89204f0a00b2a3e3d87ba2e0da525ca18388
Nominom <nomi.lakkala@gmail.com>
2019-05-24 09:42:34 +0300

    Added PC player loading to multiplayer if VR player is not loaded.

---
+++ Assets/Scripts/Utility/LoadPCSceneIfVRNotLoaded.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+/**
+ * Author: Nomi Lakkala
+ * 
+ * <summary>
+ * Checks if the VR player scene is loaded and if not, loads the PC player scene.
+ * </summary>
+ */
+public class LoadPCSceneIfVRNotLoaded : MonoBehaviour
+{
+	public string vrScene = "VRPlayer";
+	public string pcScene = "PCPlayer";
+
+	void Awake()
+	{
+		if (!SceneManager.GetSceneByName(vrScene).isLoaded)
+		{
+			Debug.Log("VRscene is not loaded. Loading PC scene", this);
+			SceneManager.LoadScene(pcScene, LoadSceneMode.Additive);
+		}
+	}
+}
