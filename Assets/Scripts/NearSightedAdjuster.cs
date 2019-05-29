29ed31a45ed1c98aa91c3ee6f12a3facb320cde1
Nominom <nomi.lakkala@gmail.com>
2019-05-29 15:56:13 +0300

    Added post processing effects for near-, and farsightedness.

---
+++ Assets/Scripts/NearSightedAdjuster.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using System.Linq;
+
+
+[RequireComponent(typeof(PostProcessVolume))]
+public class NearSightedAdjuster : MonoBehaviour
+{
+	[Range(1, 5)]
+	public float maxFocalPoint = 2;
+	public bool dofAdjustOn = false;
+
+	public PostProcessProfile normalProfile;
+	public PostProcessProfile farSightedProfile;
+	public PostProcessProfile nearSightedProfile;
+	public PostProcessProfile everythingIsBadProfile;
+
+	private PostProcessVolume volume;
+	private Camera mainCam;
+
+	private DepthOfField dof;
+
+	private Collider[] colliderArray = new Collider[10];
+	private Quaternion[] raycastAngles;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		volume = GetComponent<PostProcessVolume>();
+		raycastAngles = GetXYRotations(20, 10).ToArray();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (!dofAdjustOn)
+		{
+			return;
+		}
+		if (dof == null)
+		{
+			return;
+		}
+		if (mainCam == null)
+		{
+			mainCam = Camera.main;
+			return;
+		}
+
+		int found = Physics.OverlapSphereNonAlloc(mainCam.transform.position, maxFocalPoint, colliderArray);
+
+		if (found > 0)
+		{
+			Vector3 camF = mainCam.transform.forward;
+			Vector3 camP = mainCam.transform.position;
+			float closest = maxFocalPoint;
+			for (int i = 0; i < raycastAngles.Length; i++)
+			{
+				if (Physics.Raycast(new Ray(camP, raycastAngles[i] * camF), out RaycastHit hit, maxFocalPoint))
+				{
+					if (hit.distance < closest)
+					{
+						closest = hit.distance + 0.1f;
+					}
+				}
+			}
+			dof.focusDistance.Override(closest);
+		}
+		else
+		{
+			dof.focusDistance.Override(maxFocalPoint);
+		}
+	}
+
+
+	public void SetSightMode(EyesightMode mode)
+	{
+		dofAdjustOn = false;
+		switch (mode)
+		{
+			case EyesightMode.Normal:
+				volume.profile = normalProfile;
+				break;
+			case EyesightMode.NearSighted:
+				volume.profile = nearSightedProfile;
+				dofAdjustOn = true;
+				dof = volume.profile.GetSetting<DepthOfField>();
+				break;
+			case EyesightMode.FarSighted:
+				volume.profile = farSightedProfile;
+				break;
+			case EyesightMode.Bad:
+				volume.profile = everythingIsBadProfile;
+				break;
+			default:
+				break;
+		}
+	}
+
+
+	private void OnDrawGizmos()
+	{
+		if (!dofAdjustOn || volume == null)
+		{
+			return;
+		}
+		if (dof == null)
+		{
+			return;
+		}
+		if (mainCam == null)
+		{
+			return;
+		}
+
+		int found = Physics.OverlapSphereNonAlloc(mainCam.transform.position, maxFocalPoint, colliderArray);
+
+		if (found > 0)
+		{
+			Vector3 camF = mainCam.transform.forward;
+			Vector3 camP = mainCam.transform.position;
+
+			for (int i = 0; i < raycastAngles.Length; i++)
+			{
+				if (Physics.Raycast(new Ray(camP, raycastAngles[i] * camF), out RaycastHit hit, maxFocalPoint))
+				{
+					Gizmos.color = Color.green;
+					Gizmos.DrawLine(camP, hit.point);
+				}
+				else{
+					Gizmos.color = Color.grey;
+					Gizmos.DrawLine(camP, camP + raycastAngles[i] * camF * maxFocalPoint);
+				}
+			}
+
+		}
+	}
+
+	private IEnumerable<Quaternion> GetXYRotations(float r, int num)
+	{
+		for (int i = 0; i < num; i++)
+		{
+			yield return Quaternion.Euler(Random.insideUnitCircle * r);
+		}
+	}
+}
