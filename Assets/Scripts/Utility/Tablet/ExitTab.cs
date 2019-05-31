fc499284765911b6bb32c75e64bd5a1327068a2d
Veli-Matti Vuoti <velluv@hotmail.fi>
2019-05-31 10:00:24 +0300

    ExitEvent from ExitTab class

--- Assets/Scripts/Utility/Tablet/ExitTab.cs
+++ Assets/Scripts/Utility/Tablet/ExitTab.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ExitTab : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	Button[] buttons;
+	public delegate void ExitDelegate();
+	public static event ExitDelegate ExitEvent;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Start()
+	{
+		buttons = new Button[transform.childCount - 1];
+
+		for (int i = 0; i < transform.childCount; i++)
+		{
+			if (i != 0)
+			{
+				buttons[i-1] = transform.GetChild(i).transform.gameObject.GetComponent<Button>();
+			}
+		}
+
+		buttons[0].onClick.AddListener(() => ExitToMenu());
+		buttons[1].onClick.AddListener(() => ExitApplication());
+		
+	}
+
+	void ExitToMenu()
+	{
+		if(ExitEvent != null)
+		{
+			ExitEvent();
+		}
+
+	}
+
+	void ExitApplication()
+	{
+		if(ExitEvent != null)
+		{
+			ExitEvent();
+		}
+	}
 }
