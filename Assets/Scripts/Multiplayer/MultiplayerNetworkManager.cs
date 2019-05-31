fdc3c393e525adf5a4ff0ebc09821016794b9e41
Nominom <nomi.lakkala@gmail.com>
2019-05-31 15:06:55 +0300

    SpawnLocations check that there are no obstacles near the position

--- Assets/Scripts/Multiplayer/MultiplayerNetworkManager.cs
+++ Assets/Scripts/Multiplayer/MultiplayerNetworkManager.cs
@@ -2,6 +2,7 @@
 using Photon.Pun.UtilityScripts;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 
@@ -21,7 +22,7 @@ public class MultiplayerNetworkManager : MonoBehaviour
 
 	IEnumerator Start()
 	{
-		yield return new WaitForSeconds(0.1f);
+		yield return new WaitForSeconds(0.5f);
 		MoveLocalPlayerToSpawnLocation();
 		yield return new WaitForSeconds(1);
 		PhotonNetwork.Instantiate(avatarPrefabName, Vector3.zero, Quaternion.identity, 0);
@@ -37,6 +38,14 @@ public class MultiplayerNetworkManager : MonoBehaviour
 		var locations = FindObjectsOfType<SpawnLocation>();
 		if (locations.Length == 0) return;
 		SpawnLocation location = locations[PhotonNetwork.LocalPlayer.GetPlayerNumber() % locations.Length];
+		if (!location.IsFree())
+		{
+			location = locations.Where(x => x.IsFree()).SingleOrDefault();
+		}
+		if (location == null)
+		{
+			location = locations[0];
+		}
 
 		FindObjectOfType<PlayerPosition>().transform.position = location.transform.position;
 	}
--- Assets/Scripts/Multiplayer/SpawnLocation.cs
+++ Assets/Scripts/Multiplayer/SpawnLocation.cs
@@ -9,4 +9,28 @@ using UnityEngine;
  * Just a script for marking a spawn location.
  * </summary>
  */
-public class SpawnLocation : MonoBehaviour {}
+public class SpawnLocation : MonoBehaviour {
+	private const float yOffset = 0.1f;
+
+	[SerializeField]
+	private float collisionCheckRange = 1f;
+
+	private static Collider[] collisionBuffer = new Collider[2];
+
+	public bool IsFree(){
+		return Physics.OverlapSphereNonAlloc(transform.position + Vector3.up * (collisionCheckRange + yOffset), collisionCheckRange, collisionBuffer) == 0;
+	}
+
+	public void OnDrawGizmosSelected()
+	{
+		if (Physics.OverlapSphereNonAlloc(transform.position + Vector3.up * (collisionCheckRange + yOffset), collisionCheckRange, collisionBuffer) == 0)
+		{
+			Gizmos.color = Color.green;
+		}else{
+			Gizmos.color = Color.red;
+		}
+
+		Gizmos.DrawWireSphere(transform.position + Vector3.up * (collisionCheckRange + yOffset), collisionCheckRange);
+
+	}
+}
