cbc85e2b99aa4d1627528a0b4a45d564efd89474
Nominom <nomi.lakkala@gmail.com>
2019-06-07 09:44:27 +0300

    Added some comments

--- Assets/Scripts/MapperSystem/MapperFindOtherPlayers.cs
+++ Assets/Scripts/MapperSystem/MapperFindOtherPlayers.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * Author: Nomi Lakkala
+ * 
+ * <summary>
+ * A helper class that finds other players in multiplayer and sets mapper to track them.
+ * </summary>
+ */
 public class MapperFindOtherPlayers : MonoBehaviour
 {
 	public Mapper mapper;
