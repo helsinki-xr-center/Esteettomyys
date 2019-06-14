e87dc0ca78c2ebd7e2fec142cf30ff1d0d53c8fa
Nominom <nomi.lakkala@gmail.com>
2019-06-14 14:07:39 +0300

    Added area map and global map modes.

---
+++ Assets/Scripts/UI/Map/MapUIPanel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class MapUIPanel : MonoBehaviour
+{
+	public TextMeshProUGUI mapButtonText;
+	public string worldMapText;
+	public string areaMapText;
+
+
+	private Mapper mapper;
+	private bool worldMap = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		mapper = FindObjectOfType<Mapper>();
+		if(mapper != null)
+		{
+			SwitchToWorldMap();
+		}
+    }
+
+	
+	public void OnSwitchMapButtonClick()
+	{
+		if(worldMap)
+		{
+			SwitchToAreaMap();
+		}
+		else
+		{
+			SwitchToWorldMap();
+		}
+	}
+
+	public void SwitchToWorldMap()
+	{
+		mapper.RecalculateBounds();
+		worldMap = true;
+		mapButtonText.SetText(areaMapText);
+	}
+
+	public void SwitchToAreaMap()
+	{
+		var playerArea = FindObjectsOfType<AreaScript>().FirstOrDefault(x => x.IsPlayerInBounds());
+		if (playerArea != null)
+		{
+			mapper.SetCustomBounds(playerArea.GetBounds());
+			worldMap = false;
+			mapButtonText.SetText(worldMapText);
+		}
+	}
+}
